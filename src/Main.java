8b66a2f2f268e8b23de0c35c5c5290450c4457d1
banerjri <banerjri@mail.uc.edu>
2023-06-26 02:03:02 -0500

    For loop complete

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,56 @@
-public class Main {
+public class Main
+{
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        for (int a = 0; a <= 30; a++) {
+            System.out.printf("%3d  |", a);
+        }
+        System.out.println("\n");
+
+        for (int a = 30; a >= 0; a--) {
+            System.out.printf("%3d  |", a);
+        }
+        System.out.println("\n");
+
+        for (int a = 0; a <= 18; a += 3) {
+            System.out.printf("%3d  |", a);
+        }
+        System.out.println("\n");
+
+        for (int a = 10; a >= 0; a -= 2) {
+            System.out.printf("%3d  |", a);
+        }
+        System.out.println("\n");
+
+        for (int a = 0; a < 5; a++)
+        {
+            for (int b = 0; b <= a; b++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+        for (int a = 5; a > 0; a--)
+        {
+            for (int b = 1; b <= a; b++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+        for (int a = 5; a > 0; a--)
+        {
+            for (int b = 1; b <= 5; b++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+
     }
 }
